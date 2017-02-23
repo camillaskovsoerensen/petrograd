ed3fbffa226e9380da90547cb4a707d51603c700
Camilla Skov Sørensen <camillaskovsorensen@Camillas-MacBook-Pro-2.local>
2017-02-23 13:05:40 +0100

    load JSON produktliste and create clone for each

--- js/petrograd.js
+++ js/petrograd.js
@@ -2,12 +2,23 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    // læs pruduktliste
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktliste);
+
     visProdukt();
 
 }
 
+function visProduktliste(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
 
-function visProdukt() {
+
+function visProdukt(produkt) {
+    console.log(produkt);
     // klon produkt_template
 
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
